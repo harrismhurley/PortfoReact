0da26a465331b5b4be3fdf9731735a3cfe97230f
harrismhurley <harrismhurley@icloud.com>
2024-07-24 13:25:12 -0500

    got products lined up ad well as pages

---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Project from "../Project";
+
+function Portfolio() {
+    const [projects] = useState([
+        {
+            name: 'TextFlex',
+            description: 'Webpack PWA',
+            link: "https://textflex.onrender.com/",
+            repo: "https://github.com/harrismhurley/TextFlex"
+        },
+        {
+            name: 'SocialSphere',
+            description: 'bcrypt/PSQL',
+            link: "https://socialsphere-63b8.onrender.com",
+            repo: "https://github.com/harrismhurley/SocialSphere"
+        },
+        {
+            name: 'JotSpot',
+            description: 'Express Web App',
+            link: "https://jotspot-bf8s.onrender.com/notes",
+            repo: "https://github.com/harrismhurley/JotSpot"
+        },
+        {
+            name: 'TaskManager',
+            description: 'JavaScript/JQueryUI',
+            link: "https://harrismhurley.github.io/Task_ManagerHQ/",
+            repo: "https://github.com/harrismhurley/Task_ManagerHQ"
+        },
+        {
+            name: 'WhiteSandBeachBlog',
+            description: 'Javssctipt/CSS',
+            link: "https://harrismhurley.github.io/White-Sand-Beach-Blog/",
+            repo: "https://github.com/harrismhurley/White-Sand-Beach-Blog"
+        },
+
+    ]);
+};
